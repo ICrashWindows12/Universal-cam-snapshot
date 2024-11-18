85bddda86f87740b49c2c5b58131796d48e63e16
Icrashwin12 <73936448+ICrashWindows12@users.noreply.github.com>
2024-11-18 23:06:17 +1100

    Update outputscript.sh

--- images/outputscript.sh
+++ images/outputscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # RTSP stream URL
-RTSP_URL="rtsp://user:password@0.0.0.0:PORT/examplestream"
+RTSP_URL="rtsp://username:password@0.0.0.0:PORT/examplestream" # REPLACE THIS WITH YOUR ACTUAL RTSP URL
 # Directory to save the current image
 IMAGE_DIR="/var/www/html/images"
 # Current image filename
@@ -10,16 +10,33 @@ CURRENT_IMAGE="current_status.jpeg"
 # Create the directory if it doesn't exist
 mkdir -p "$IMAGE_DIR"
 
-while true; do
-    # Capture a single frame from the RTSP stream and save it as current_status.jpeg
-    ffmpeg -rtsp_transport tcp -i "$RTSP_URL" -vframes 1 "$IMAGE_DIR/$CURRENT_IMAGE" -y
+# Ask user whether to preserve snapshots or discard them
+read -p "Do you want to preserve snapshots? (y/n): " choice
 
-    # Wait for 60 seconds before capturing the next frame
-    sleep 60.02
+if [[ "$choice" == "y" ]]; then
+    # Preserve Snapshots
+    while true; do
+        # rename a single frame from the RTSP stream and save it as current_status_DD/MM/YY.jpeg
+         TIMESTAMP=$(date +'%d-%m-%Y_%H-%M-%S')
+        mv "$IMAGE_DIR/$CURRENT_IMAGE" "$IMAGE_DIR/current_status_$TIMESTAMP.jpeg"
 
-    # After 70 seconds, delete the current image (this will delete the image after 70 seconds from the last capture)
-    if [ -f "$IMAGE_DIR/$CURRENT_IMAGE" ]; then
-        sleep 0.01  # Wait an additional 10 seconds (70 seconds total since last capture)
-        rm "$IMAGE_DIR/$CURRENT_IMAGE"
-    fi
-done
+        # Wait for 1 seconds before taking snapshot
+        sleep 1
+
+        # Rename the current image with a timestamp
+         # Capture a single frame from the RTSP stream and save it as current_status.jpeg
+        ffmpeg -rtsp_transport tcp -i "$RTSP_URL" -vframes 1 "$IMAGE_DIR/$CURRENT_IMAGE" -y
+
+        # Wait for 60 second before renaming the current status image
+        sleep 60
+    done
+else
+    # Discard Snapshots
+    while true; do
+        # Capture a single frame from the RTSP stream and save it as current_status.jpeg
+        ffmpeg -rtsp_transport tcp -i "$RTSP_URL" -vframes 1 "$IMAGE_DIR/$CURRENT_IMAGE" -y
+
+        # Wait for 60 seconds before capturing the next frame
+        sleep 60
+    done
+fi
